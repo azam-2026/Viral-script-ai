66524b5ae0645fcc3dfdf64b3a86a86969ab8031
azam-2026 <khanazam20804@gmail.com>
2026-09-20 13:33:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ demo = gr.Interface(
 if __name__ == "__main__":
     # Render dynamic port fix
     port = int(os.environ.get("PORT", 7860))
-    demo.launch(server_name="0.0.0.0", server_port=port
+    demo.launch(server_name="0.0.0.0",
